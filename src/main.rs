ae4a335e811dab9629fee08f1430453913e9458f
Joseph Edwards <136659064+leisurelyleon@users.noreply.github.com>
2025-08-07 10:37:57 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use paradime_main::load_genesis;
+
 fn main() {
-    use paradime_main::load_genesis;
-    printIn!(
-        "Genesis loaded: max={}, total{}, circ={}",
-        genesis.max_supply, genesis.total_supply, genesis.circulating_supply
+    // Load the genesis configuration
+    let genesis = load_genesis("genesis.json");
+
+    // Print out the loaded supply parameters
+    println!(
+        "Genesis loaded: max={}, total={}, circ={}",
+        genesis.max_supply,
+        genesis.total_supply,
+        genesis.circulating_supply
     );
-    // TODO: Kick off your node initialization...
+
+    // TODO: Initialize node, start consensus engine, spin up RPC server…
 }
